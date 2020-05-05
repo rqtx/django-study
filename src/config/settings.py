3165ba1e117e7733e2dd0ccd334d1e638a7d8931
Igor Fernandes Miranda <igormiranda5@gmail.com>
2020-05-05 20:39:03 -0300

    Django secret

--- src/config/settings.py
+++ src/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lp8c!nrb_tt&ga_z&-q)c@#si$87w!773n&gtzf^)3k=96-np+'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
